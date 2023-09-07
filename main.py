d709784b565e788b187c1cbe1c4c357b5685bc2f
luxkatana <taseen.bibi@gmail.com>
2023-09-07 22:05:49 +0200

    somtodayptyhon

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 from fastapi import FastAPI, Response
 import uvicorn
+import schedule
+import somtodaypython.nonasyncsomtoday as somtodaypython
 from threading import Thread
 def main() -> None:
-    pass
+    while True:
+        pass
 
-main_background = Thread()
+main_background = Thread(target=main)
 
 app = FastAPI()
 
 
 @app.get('/heartbeat/')
 async def heartbeat():
-    return Response("{\"response\": true}", media_type='application/json')
+    school = somtodaypython.find_school("Hondsrug College")
+    student = school.get_student("133600", 'inchem2009')
+    print(student)
+    return Response("{\"response\": \"%s\"}" % student.full_name, media_type='application/json')
 
 if __name__ ==  "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
