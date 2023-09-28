a7921889ccadd9a2b550e36e12e1fc2ad3e54cb7
luxkatana <taseen.bibi@gmail.com>
2023-09-28 21:44:06 +0200

    long

--- main.py
+++ main.py
@@ -1,25 +1,79 @@
 from fastapi import FastAPI, Response
+from json import dumps
+import logging
+import pytz
+from datetime import datetime, timedelta 
+import requests
 import uvicorn
-import schedule
-import somtodaypython.nonasyncsomtoday as somtodaypython
-# o
+import somtodaypython.nonasyncsomtoday as somtoday
+from time import sleep as wait
 from threading import Thread
+logging.basicConfig(filename="main_server_log.log", filemode='a', level=logging.ERROR,
+                    format="%(asctime)s: %(message)s")
+CEST = pytz.FixedOffset(120)
+school = somtoday.find_school("Hondsrug College")
+
+def get_nearest_time(subjects: list[somtoday.Subject],
+                     now:  datetime) -> tuple[somtoday.Subject]:
+    def check(subject:  somtoday.Subject) -> bool:
+        return \
+            subject.begin_time > now and\
+            (offsetby10 := (
+                subject.begin_time - timedelta(minutes=10))).hour == now.hour \
+            and offsetby10.minute == now.minute
+                
+            
+    result = tuple(filter(check,  subjects))
+    return result
+
+
+    
 def main() -> None:
     while True:
-        pass
+        with open("./main_server_log.log", 'r') as f:
+            if len(f.readline()) >= 100:
+                f.truncate()
+        student = school.get_student('133600', 'inchem2009')
+        now = datetime.now(CEST)
+        # now = datetime(2023, 9, 11, 9, 50, tzinfo=CEST)
+        rooster: list[somtoday.Subject] = student\
+            .fetch_schedule(now, now + timedelta(days=1))
+        # rooster: [Subject, Subject, ...]
+        nearest_subject_now  = get_nearest_time(rooster, now)
+        if nearest_subject_now !=  ():
+            vak = nearest_subject_now[0]
+            response = requests.post("https://ntfy.sh/",
+                       data=dumps({
+                           'topic': 'luxkatanaschool133600',
+                           'title':  f'{vak.subject_name} ({vak.begin_hour}{"ste" if vak.begin_hour in [1, 8] else "de"} uur) gaat zo over 10 minuten beginnen!',
+                           'message': f'begint om {vak.begin_time.strftime("%H:%M")} lokaal {vak.location}',
+                           "priority": 4,
+                       }
+                           ),
+                       )
+            if response.status_code == 200:
+                logging.error("Message has been successfully sent")
+            else:
+                logging.error(f"Response returned status code {response.status_code} with error: {response.text}")
+            
+        else:
+            logging.error(f"Condition was not correct {nearest_subject_now}\t {now}")
+        wait(60)
 
-main_background = Thread(target=main)
 
 app = FastAPI()
 
 
 @app.get('/heartbeat/')
 async def heartbeat():
-    school = somtodaypython.find_school("Hondsrug College")
-    student = school.get_student("133600", 'inchem2009')
-    print(student)
-    return Response("{\"response\": \"%s\"}" % student.full_name, media_type='application/json')
+    return Response("{\"response\": true}", media_type='application/json')
 
 if __name__ ==  "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-    print("Application started")
+    Thread(target=main, daemon=True).start()
+    print("Server on 0.0.0.0:40623 is on!")
+    uvicorn.run(app, host="0.0.0.0", port=40263, log_level=logging.ERROR)
+    # uvicorn.run(app, host="127.0.0.1", port=8000, log_level=logging.ERROR)
+    '''
+    127.0.0.1:8000 -> test-server
+    0.0.0.0 -> 40263 development-server (production)
+    '''
